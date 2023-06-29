311211c910932b2333d40305fff44a613d7436d3
temeculous <zacerious@gmail.com>
2023-06-29 14:51:47 -0400

    Added tools and wallet variable

---
+++ game.py
@@ -0,0 +1,3 @@
+tools = {"rustyScissors": {"cost": 5, "earnings": 5}, "oldMower": {"cost": 25, "earnings": 50}, "newMower": {"cost":250, "earnings": 100}, "students": {"cost": 500, "earnings": 250}}
+
+wallet = 0
